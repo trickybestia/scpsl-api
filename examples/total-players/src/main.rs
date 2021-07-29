15b40d6c7cbce041d49eedc6537e5b5eeb108533
trickybestia <trickybestia@gmail.com>
2021-07-29 16:26:51 +0300

    add example

---
+++ examples/total-players/src/main.rs
@@ -0,0 +1,32 @@
+use scpsl_api::server_info::{get, RequestParameters, Response};
+use std::env::var;
+use url::Url;
+
+#[tokio::main]
+async fn main() {
+    let account_id = var("ACCOUNT_ID")
+        .expect("Expected an account id in the environment")
+        .parse::<u64>()
+        .unwrap();
+    let api_key = var("API_KEY").expect("Expected an account id in the environment");
+
+    let url = Url::parse("https://api.scpslgame.com/serverinfo.php").unwrap();
+
+    let parameters = RequestParameters::builder()
+        .url(&url)
+        .id(account_id)
+        .key(api_key.as_str())
+        .players(true)
+        .build();
+
+    if let Response::Success(response) = get(&parameters).await.unwrap() {
+        println!(
+            "Total players on your servers: {}",
+            response
+                .servers()
+                .iter()
+                .map(|server| server.players_count().unwrap().current_players())
+                .sum::<u32>()
+        )
+    }
+}
