6385923645b07a79bffb8b06d37103687f0fbb96
trickybestia <trickybestia@gmail.com>
2021-07-30 12:10:43 +0300

    imlement trait std::error::Error for ip::Error

--- src/ip.rs
+++ src/ip.rs
@@ -2,17 +2,38 @@
 //! working with the `ip` API request.
 
 use std::{
-    net::{AddrParseError, IpAddr},
+    error::Error as StdError,
+    fmt::{Display, Formatter},
+    net::IpAddr,
     str::FromStr,
 };
 use url::Url;
 
 /// An enum representing an error for the `ip` request.
+#[derive(Debug)]
 pub enum Error {
     /// An enum variant representing [`AddrParseError`].
-    AddrParseError(AddrParseError),
+    AddrParseError(),
     /// An enum variant representing [`reqwest::Error`].
     ReqwestError(reqwest::Error),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::AddrParseError() => write!(f, "could not parse ip address from the response"),
+            Error::ReqwestError(error) => write!(f, "reqwest error: `{}`", error),
+        }
+    }
+}
+
+impl StdError for Error {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        match self {
+            Error::ReqwestError(error) => Some(error),
+            _ => None,
+        }
+    }
 }
 
 /// Returns current ip.
@@ -24,7 +45,7 @@ pub async fn get(url: Url) -> Result<IpAddr, Error> {
         Ok(response) => match response.text().await {
             Ok(text) => match IpAddr::from_str(text.as_str()) {
                 Ok(ip) => Ok(ip),
-                Err(error) => Err(Error::AddrParseError(error)),
+                Err(_) => Err(Error::AddrParseError()),
             },
             Err(error) => Err(Error::ReqwestError(error)),
         },
